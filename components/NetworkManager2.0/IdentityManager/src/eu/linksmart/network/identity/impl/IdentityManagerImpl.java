2993d920fb9a067e0052633a758ff9f20c1f4653
hansmuff2 <hansmuff2@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2011-11-14 15:46:41 +0000

    prototypical implementation
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@66 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/NetworkManager2.0/IdentityManager/src/eu/linksmart/network/identity/impl/IdentityManagerImpl.java
+++ components/NetworkManager2.0/IdentityManager/src/eu/linksmart/network/identity/impl/IdentityManagerImpl.java
@@ -1,22 +1,55 @@
 package eu.linksmart.network.identity.impl;
 
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
 import org.osgi.service.component.ComponentContext;
 
+import eu.linksmart.network.HID;
 import eu.linksmart.network.identity.IdentityManager;
 
 
 /*
  * TODO #NM refactoring
  */
-public class IdentityManagerImpl implements IdentityManager{
-	private static String IDENTITY_MGR = IdentityManagerImpl.class.getSimpleName();
+public class IdentityManagerImpl implements IdentityManager {
+	private static String IDENTITY_MGR = IdentityManagerImpl.class
+			.getSimpleName();
 	
-protected void activate(ComponentContext context) {
+	private Map<HID, Map<HIDAttribute, String>> hidTable;
 	
-	System.out.println(IDENTITY_MGR + "started");
-	
-}
-protected void deactivate(ComponentContext context) {
-	System.out.println(IDENTITY_MGR + "stopped");
-}
+	private Logger LOG = Logger.getLogger(IdentityManagerImpl.class); 
+		
+	protected void activate(ComponentContext context) {
+		LOG.info(IDENTITY_MGR + "started");
+	}
+
+	protected void deactivate(ComponentContext context) {
+		LOG.info(IDENTITY_MGR + "stopped");
+	}
+
+	@Override
+	public HID createHID(Map<HIDAttribute, String> attributes) {
+		// TODO Auto-generated method stub
+		HID newHID = null; //TODO generate this HID!
+		
+		hidTable.put(newHID, attributes);
+		return newHID;
+	}
+
+	@Override
+	public Set<HID> getHIDs(Map<HIDAttribute, String> attributes) {
+		// TODO Auto-generated method stub
+		Set<HID> searchResults = null;
+		
+		for (HID hid : hidTable.keySet()) {
+			/*TODO real check: hidTable.get(hid) matches attributes*/
+			if (false ) {
+				searchResults.add(hid);
+			}
+		}
+		
+		return searchResults;
+	}
 }
