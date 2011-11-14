c140de427c3acaab0c548cd2e8479f5c4a9e8c90
schneider-fit <schneider-fit@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2011-11-14 15:12:30 +0000

    Fixed source formatting.
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@61 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/routing/BackboneRouter.java
+++ components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/routing/BackboneRouter.java
@@ -2,23 +2,21 @@ package eu.linksmart.network.routing;
 
 import eu.linksmart.network.HID;
 import eu.linksmart.network.Message;
+import eu.linksmart.network.backbone.Backbone;
 
 /*
  * The BackboneRouter is responsible for selecting the correct channel to send the message according to the receiverHID.
  */
 public interface BackboneRouter {
-	
-	
+
 	/**
-	 * This method checks by which channel the receiver is reachable and sends the message.
+	 * This method checks by which channel the receiver is reachable and sends
+	 * the message.
 	 * 
 	 * @param senderHID
 	 * @param receiverHID
 	 * @param message
 	 */
 	public void sendData(HID senderHID, HID receiverHID, Message message);
-	
-
-	
 
 }
