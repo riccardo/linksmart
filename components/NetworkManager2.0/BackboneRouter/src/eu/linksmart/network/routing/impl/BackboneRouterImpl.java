e597b873021068cc679f5b945a84e8c6bb37d32e
schneider-fit <schneider-fit@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2011-11-14 14:57:35 +0000

    Implemented methods from interface definition without actual content. Added network library dependency in manifest files.
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@58 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/NetworkManager2.0/BackboneRouter/src/eu/linksmart/network/routing/impl/BackboneRouterImpl.java
+++ components/NetworkManager2.0/BackboneRouter/src/eu/linksmart/network/routing/impl/BackboneRouterImpl.java
@@ -2,30 +2,50 @@ package eu.linksmart.network.routing.impl;
 
 import org.osgi.service.component.ComponentContext;
 
+import eu.linksmart.network.HID;
+import eu.linksmart.network.Message;
 import eu.linksmart.network.backbone.Backbone;
 import eu.linksmart.network.routing.BackboneRouter;
-
 
 /*
  * TODO #NM refactoring
  */
-public class BackboneRouterImpl implements BackboneRouter{
-	private static String BACKBONE_ROUTER = BackboneRouterImpl.class.getSimpleName();
+public class BackboneRouterImpl implements BackboneRouter {
 	
-protected void activate(ComponentContext context) {
-	
-	System.out.println(BACKBONE_ROUTER + "started");
-	
-}
-protected void deactivate(ComponentContext context) {
-	System.out.println(BACKBONE_ROUTER + "stopped");
-}
+	private static String BACKBONE_ROUTER = BackboneRouterImpl.class
+			.getSimpleName();
 
-protected void bindBackbone(Backbone backbone){
-	
-}
+	protected void activate(ComponentContext context) {
+		System.out.println(BACKBONE_ROUTER + "started");
+	}
 
-protected void unbindBackbone(Backbone backbone){
+	protected void deactivate(ComponentContext context) {
+		System.out.println(BACKBONE_ROUTER + "stopped");
+	}
+
+	protected void bindBackbone(Backbone backbone) {
+
+	}
+
+	protected void unbindBackbone(Backbone backbone) {
+
+	}
 	
-}
+	
+	/**
+	 * This method checks by which channel the receiver is reachable and sends the message.
+	 * 
+	 * @param senderHID
+	 * @param receiverHID
+	 * @param message
+	 */
+	public void sendData(HID senderHID, HID receiverHID, Message message) {
+		
+	}
+	
+
+	
+
+	
+	
 }
--- components/NetworkManager2.0/BackboneWS/src/eu/linksmart/network/backbone/impl/ws/BackboneWSImpl.java
+++ components/NetworkManager2.0/BackboneWS/src/eu/linksmart/network/backbone/impl/ws/BackboneWSImpl.java
@@ -2,6 +2,8 @@ package eu.linksmart.network.backbone.impl.ws;
 
 import org.osgi.service.component.ComponentContext;
 
+import eu.linksmart.network.HID;
+import eu.linksmart.network.Message;
 import eu.linksmart.network.backbone.Backbone;
 
 /*
@@ -9,13 +11,39 @@ import eu.linksmart.network.backbone.Backbone;
  */
 public class BackboneWSImpl implements Backbone {
 	private static String BACKBONE_WS = BackboneWSImpl.class.getSimpleName();
+
+	protected void activate(ComponentContext context) {
+		System.out.println(BACKBONE_WS + "started");
+	}
+
+	protected void deactivate(ComponentContext context) {
+		System.out.println(BACKBONE_WS + "stopped");
+	}
+
 	
-protected void activate(ComponentContext context) {
+	/**
+	 * Sends a message over the webservice communication channel.
+	 * 
+	 * @param senderHID
+	 * @param receiverHID
+	 * @param message
+	 */
+	public void sendData(HID senderHID, HID receiverHID, Message message) {
+		
+	}
 	
-	System.out.println(BACKBONE_WS + "started");
+
 	
-}
-protected void deactivate(ComponentContext context) {
-	System.out.println(BACKBONE_WS + "stopped");
-}
+	/**
+	 * Receives a message over the webservice communication channel.
+	 * 
+	 * @param senderHID
+	 * @param receiverHID
+	 * @param message
+	 */
+	public void receiveData(HID senderHID, HID receiverHID, Message message) {
+		
+	}
+
+
 }
