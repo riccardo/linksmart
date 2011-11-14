f2464e625cdae90997173c62db146912625fb122
hansmuff2 <hansmuff2@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2011-11-14 14:42:13 +0000

    added for creating and getting HIDs
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@56 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/identity/IdentityManager.java
+++ components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/identity/IdentityManager.java
@@ -1,8 +1,17 @@
 package eu.linksmart.network.identity;
 
+import java.util.Map;
+
+import eu.linksmart.network.HID;
+import eu.linksmart.network.HID.HIDAttributes;
+
+
 /*
  * TODO #NM refactoring
  */
 public interface IdentityManager {
-
+		
+	public HID createHID(Map<HIDAttributes, String> attributes);
+	
+	public HID getHID(Map<HIDAttributes, String> attributes);
 }
