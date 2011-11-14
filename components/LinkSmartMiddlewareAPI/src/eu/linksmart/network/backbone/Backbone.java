1f3642265755f2c89516c0503aab5f33ded4aad6
schneider-fit <schneider-fit@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2011-11-14 14:49:08 +0000

    Added methods for Backbone interfaces.
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@57 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/backbone/Backbone.java
+++ components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/backbone/Backbone.java
@@ -1,8 +1,33 @@
 package eu.linksmart.network.backbone;
 
+import eu.linksmart.network.HID;
+import eu.linksmart.network.Message;
+
 /*
- * TODO #NM refactoring
+ * A Backbone needs to be implemented if a new communication channel (e.g. P2P, JMS, ...) should be used by LinkSmart.
  */
 public interface Backbone {
+	
+	
+	/**
+	 * Sends a message over the specific communication channel.
+	 * 
+	 * @param senderHID
+	 * @param receiverHID
+	 * @param message
+	 */
+	public void sendData(HID senderHID, HID receiverHID, Message message);
+	
 
+	
+	/**
+	 * Receives a message over the specific communication channel.
+	 * 
+	 * @param senderHID
+	 * @param receiverHID
+	 * @param message
+	 */
+	public void receiveData(HID senderHID, HID receiverHID, Message message);
+
+	
 }
--- components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/routing/BackboneRouter.java
+++ components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/routing/BackboneRouter.java
@@ -1,8 +1,24 @@
 package eu.linksmart.network.routing;
 
+import eu.linksmart.network.HID;
+import eu.linksmart.network.Message;
+
 /*
- * TODO #NM refactoring
+ * The BackboneRouter is responsible for selecting the correct channel to send the message according to the receiverHID.
  */
 public interface BackboneRouter {
+	
+	
+	/**
+	 * This method checks by which channel the receiver is reachable and sends the message.
+	 * 
+	 * @param senderHID
+	 * @param receiverHID
+	 * @param message
+	 */
+	public void sendData(HID senderHID, HID receiverHID, Message message);
+	
+
+	
 
 }
