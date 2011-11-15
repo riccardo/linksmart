527c73a13469d485360336ca3cf096be9f5098e7
tranten <tranten@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2011-11-15 11:13:50 +0000

    CommunicationSecurityManager bundle implements interface TrustManager added as referenced service
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@84 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/NetworkManager2.0/CommunicationSecurityManager/src/eu/linksmart/security/communication/impl/CommunicationSecurityManagerImpl.java
+++ components/NetworkManager2.0/CommunicationSecurityManager/src/eu/linksmart/security/communication/impl/CommunicationSecurityManagerImpl.java
@@ -1,15 +1,19 @@
 package eu.linksmart.security.communication.impl;
 
-
 import org.osgi.service.component.ComponentContext;
 
 import eu.linksmart.security.communication.CommunicationSecurityManager;
+import eu.linksmart.security.communication.SecurityProtocol;
 import eu.linksmart.security.cryptomanager.CryptoManager;
+import eu.linksmart.security.trustmanager.TrustManager;
 
 
 
-/*
- * TODO #NM refactoring
+/**
+ * Provides {@link SecurityProtocol} implementations of
+ * specific type to be used for communication protection.
+ * @author Vinkovits
+ *
  */
 public class CommunicationSecurityManagerImpl implements CommunicationSecurityManager{
 	private static String COMMUNICATION_SEC_MGR = CommunicationSecurityManagerImpl.class.getSimpleName();
@@ -17,13 +21,30 @@ public class CommunicationSecurityManagerImpl implements CommunicationSecurityMa
 protected void activate(ComponentContext context) {
 	
 	System.out.println(COMMUNICATION_SEC_MGR + "started");
-	
 }
 protected void deactivate(ComponentContext context) {
 	System.out.println(COMMUNICATION_SEC_MGR + "stopped");
 }
 
-protected void bindCryptoManager(CryptoManager cryptoManager){}
-protected void unbindCryptoManager(CryptoManager cryptoManager){}
+protected void bindCryptoManager(CryptoManager cryptoManager){
+	
+}
+
+protected void unbindCryptoManager(CryptoManager cryptoManager){
+	
+}
+
+protected void bindTrustManager(TrustManager trustManager){
+	//TODO #NM refactoring TrustManager should also be reachable over url
+}
+
+protected void unbindTrustManager(TrustManager trustManager){
+	
+}
+
+public SecurityProtocol getSecurityProtocol() {
+	// TODO Auto-generated method stub
+	return null;
+}
 
 }
