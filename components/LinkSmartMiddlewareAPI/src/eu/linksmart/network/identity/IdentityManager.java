c705cd563ab7e150c2d5731fd6586384f57f9f08
hansmuff2 <hansmuff2@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2011-11-16 13:53:39 +0000

    updated interface
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@122 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/identity/IdentityManager.java
+++ components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/identity/IdentityManager.java
@@ -1,10 +1,11 @@
 package eu.linksmart.network.identity;
 
-import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
+import java.util.Vector;
 
 import eu.linksmart.network.HID;
+import eu.linksmart.network.HIDInfo;
 
 
 /*
@@ -19,4 +20,10 @@ public interface IdentityManager {
 	public HID createHID(Properties attributes);
 	
 	public Set<HID> getHIDs(Properties attributes);
+	
+	public Vector<String> getHIDsByAttributes(String query, int maxNum);
+	
+	public Vector<String> getHIDsByDescription(String description);
+	
+	public  HIDInfo getHIDInfo(HID hid);
 }
