52ff656cfdd62da9b8aacfe34c4fcac82dd36b42
amorotti <amorotti@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2011-11-17 15:35:22 +0000

    service registry interface
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@187 95b9cf05-cb7e-4607-9643-a8f9ddef502b

---
+++ components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/service/registry/ServiceRegistry.java
@@ -0,0 +1,17 @@
+package eu.linksmart.network.service.registry;
+
+import java.net.URL;
+
+import eu.linksmart.network.HID;
+
+public interface ServiceRegistry {
+	
+	public boolean registerService(HID hid, URL url);
+	
+	public boolean removeService(HID hid);
+	
+	public boolean updateServiceHID(URL url);
+	
+	public URL getServiceURL();
+
+}
