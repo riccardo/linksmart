29e8d92b44ca3cdefc0e4c2be5b45036369257d1
hansmuff2 <hansmuff2@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2011-11-17 14:11:19 +0000

    add method to remove an HID
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@176 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/identity/IdentityManager.java
+++ components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/identity/IdentityManager.java
@@ -5,31 +5,53 @@ import java.util.Set;
 
 import eu.linksmart.network.HID;
 import eu.linksmart.network.HIDInfo;
-import eu.linksmart.network.Message;
 
-
-/*
- * TODO #NM refactoring
+/**
+ * The IdentityManager is responsible for creating and storing HIDs.
+ * 
+ * 
  */
 public interface IdentityManager {
-	
+
+	/**
+	 * Attributes describing an HID. 
+	 *
+	 */
 	public enum HIDAttribute {
 		PID, SID, DESCRIPTION
 	}
-		
+
+	/**
+	 * Creates an HID with the given attributes. Attributes should be one of
+	 * {@link HIDAttribute}
+	 * 
+	 * @param attributes Attributes describing the HID. E.g. description.
+	 * @return
+	 */
 	public HID createHID(Properties attributes);
+
 	public boolean updateHIDInfo(HID hid, Properties attr);
-	//public Set<HID> getHIDs(Properties attributes);
-	
-	//public HID createHID(long contextID, int level, String description, String endpoint, Properties attr);
-	//public HID createHID(long contextID, int level);
-	//public HID createHID(String description, String endpoint);
-	//public HID createHID();
-	//public HID createHID(String description, String endpoint, Properties attr);
+
+	// public Set<HID> getHIDs(Properties attributes);
+
+	// public HID createHID(long contextID, int level, String description,
+	// String endpoint, Properties attr);
+	// public HID createHID(long contextID, int level);
+	// public HID createHID(String description, String endpoint);
+	// public HID createHID();
+	// public HID createHID(String description, String endpoint, Properties
+	// attr);
 	public HIDInfo getHIDInfo(HID hid);
+
 	public Set<HIDInfo> getAllHIDs();
+
 	public Set<HIDInfo> getLocalHIDs();
+
 	public Set<HIDInfo> getRemoteHIDs();
+
 	public Set<HIDInfo> getHIDsByDescription(String description);
+
 	public Set<HIDInfo> getHIDsByAttributes(String query, int maxNum);
+	
+	public boolean removeHID(HID hid);
 }
