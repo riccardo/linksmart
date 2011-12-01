cece7855d3c54015cfb65601a757f3a974957ef3
hydradox <hydradox@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2011-12-01 10:29:00 +0000

    extract methods to remove duplicated code
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@250 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/NetworkManager2.0/NetworkManager/src/eu/linksmart/tools/NetworkManagerApplicationStatus.java
+++ components/NetworkManager2.0/NetworkManager/src/eu/linksmart/tools/NetworkManagerApplicationStatus.java
@@ -36,6 +36,7 @@ package eu.linksmart.tools;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.util.Enumeration;
 import java.util.Iterator;
@@ -64,118 +65,100 @@ public class NetworkManagerApplicationStatus extends HttpServlet {
 	 * Generated serial version UID
 	 */
 	private static final long serialVersionUID = -159804849704448746L;
-	
+
 	IdentityManager identityManager;
 	ComponentContext context;
 	private BackboneRouter backboneRouter;
 	private ServiceRegistry serviceRegistry;
-	
+
 	/**
 	 * Constructor
 	 * 
-	 * @param context the bundle's context
-	 * @param networkManagerCore the Network Manager Service implementation
+	 * @param context
+	 *            the bundle's context
+	 * @param networkManagerCore
+	 *            the Network Manager Service implementation
 	 */
-	public NetworkManagerApplicationStatus(ComponentContext context, 
-			NetworkManagerCore networkManager, IdentityManager identityManager, BackboneRouter backboneRouter, ServiceRegistry serviceRegistry) {
-		
-		this.identityManager =  identityManager;
+	public NetworkManagerApplicationStatus(ComponentContext context,
+			NetworkManagerCore networkManager, IdentityManager identityManager,
+			BackboneRouter backboneRouter, ServiceRegistry serviceRegistry) {
+
+		this.identityManager = identityManager;
 		this.backboneRouter = backboneRouter;
 		this.context = context;
 	}
-	
+
 	/**
 	 * Performs the HTTP GET operation
 	 * 
-	 * @param request HttpServletRequest that encapsulates the request to the servlet 
-	 * @param response HttpServletResponse that encapsulates the response from the servlet
+	 * @param request
+	 *            HttpServletRequest that encapsulates the request to the
+	 *            servlet
+	 * @param response
+	 *            HttpServletResponse that encapsulates the response from the
+	 *            servlet
 	 */
-	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
-		
+
 		response.setContentType("text/html");
-		URL cssFile = context.getBundleContext().getBundle().getResource(
-			"resources/NetworkManagerCore.css");
+		URL cssFile = context.getBundleContext().getBundle()
+				.getResource("resources/NetworkManagerCore.css");
 		BufferedReader cssReader = new BufferedReader(new InputStreamReader(
-			cssFile.openStream()));
-		
+				cssFile.openStream()));
+
 		String temp;
 		String css = "";
 		while ((temp = cssReader.readLine()) != null) {
 			css = css + temp;
 		}
 		cssReader.close();
-		
-		response.getWriter().println("<html><head>"
-			+ "<style type=\"text/css\">" + css +"</style></head>");
-		response.getWriter().println("<body><table><tr>"
-			+ "<td valign=\"middle\" width=80%><h1>"
-			+ "Status page for the local Network Manager</h1></td>" 
-			+ "<td align=\"right\" width=20%>"
-			+ "<img src=\"files/0.gif\" /></td></tr></table>");
-		response.getWriter().println("<h1>" + "Total Number of HIDs in the "
-			+ "LinkSmart Network: " + identityManager.getAllHIDs().size() + "</h1>");
-		
+
+		PrintWriter writer = response.getWriter();
+		writer.println("<html><head>" + "<style type=\"text/css\">" + css
+				+ "</style></head>");
+		writer.println("<body><table><tr>"
+				+ "<td valign=\"middle\" width=80%><h1>"
+				+ "Status page for the local Network Manager</h1></td>"
+				+ "<td align=\"right\" width=20%>"
+				+ "<img src=\"files/0.gif\" /></td></tr></table>");
+		writer.println("<h1>" + "Total Number of HIDs in the "
+				+ "LinkSmart Network: " + identityManager.getAllHIDs().size()
+				+ "</h1>");
+
 		/* Print Network Managers Discovered. */
-		response.getWriter().println("<table border=1 class=\"stats\" "
-			+ "width=100%><tr><td class=\"hed\" width=25%>NETWORK MANAGERS</td>"
-			+ "<td class=\"hed\" width=25%>DESCRIPTION</td><td class=\"hed\" "
-			+ "width=25%>HOST</td><td class=\"hed\" width=25%>ENDPOINT</td></tr>");
-		
-		String s = "";
-		Set<HIDInfo> hids = identityManager.getHIDsByDescription("NetworkManager*");
+		writer.println(buildHidTable("NETWORK MANAGERS",
+				identityManager.getHIDsByDescription("NetworkManager*"), false));
+
+		/* Print Local HIDs. */
+		writer.println(buildHidTable("LOCALHIDS", identityManager.getLocalHIDs(), true));
+
+		/* Print All HIDs. */
+		writer.println(buildHidTable("HID", identityManager.getAllHIDs(), false));
+	}
+
+	private String processHIDs(Set<HIDInfo> hids, boolean checkAttributes) {
 		Iterator<HIDInfo> it = hids.iterator();
+		String s = "";
 		String endpoint;
 		String description;
 		String route;
-		
 		while (it.hasNext()) {
 			HID hid = new HID(it.next().getHID());
-			
+
 			try {
-				
+
 				endpoint = this.serviceRegistry.getServiceURL(hid).toString();
 				if (endpoint.equals(" ")) {
 					endpoint = "-";
 				}
 				description = identityManager.getHIDInfo(hid).getDescription();
-				route = this.backboneRouter.getRoute(hid);
-				s = s + "<tr><td width=25%>" + hid.toString()
-					+ "</td><td width=25%>" + description
-					+ "</td><td width=25%>" + route
-					+ "</td><td width=25%>" + endpoint + "</td></tr>";
-			}
-			catch (Exception e) {
-				System.out.println("Error printing " + hid);
-			}
-		}
-		response.getWriter().println(s);						
-		response.getWriter().println("</table>");
-		
-		/* Print Local HIDs. */
-		response.getWriter().println("<table border=1 class=\"stats\" width=100%>"
-			+ "<tr><td class=\"hed\" width=25%>LOCALHIDS</td>"
-			+ "<td class=\"hed\" width=25%>DESCRIPTION</td>"
-			+ "<td class=\"hed\" width=25%>HOST</td>"
-			+ "<td class=\"hed\" width=25%>ENDPOINT</td></tr>");
-		s = "";
-		hids = identityManager.getLocalHIDs();
-		it = hids.iterator();
-		
-		while (it.hasNext()) {
-			HID hid = new HID(it.next().getHID());
-			try {
-				endpoint = this.serviceRegistry.getServiceURL(hid).toString();
-				if (endpoint.equals(" ")) {
-					endpoint = "-";
-				}
-				description = identityManager.getHIDInfo(hid).getDescription();
-				
-				if (description.equals("")) {
+
+				if (checkAttributes && description.equals("")) {
 					HIDInfo hidInfo = identityManager.getHIDInfo(hid);
-					
+
 					Properties attr = hidInfo.getAttributes();
-					
+
 					Enumeration<Object> en = attr.keys();
 					while (en.hasMoreElements()) {
 						String key = (String) en.nextElement();
@@ -184,53 +167,34 @@ public class NetworkManagerApplicationStatus extends HttpServlet {
 							continue;
 						}
 						String value = attr.getProperty(key);
-						description = description + key + " = " + value + "<br/>";
+						description = description + key + " = " + value
+								+ "<br/>";
 					}
-										
-					
+
 				}
+
 				route = this.backboneRouter.getRoute(hid);
 				s = s + "<tr><td width=25%>" + hid.toString()
-					+ "</td><td width=25%>" + description
-					+ "</td><td width=25%>" + route
-					+ "</td><td width=25%>" + endpoint + "</td></tr>";
+						+ "</td><td width=25%>" + description
+						+ "</td><td width=25%>" + route + "</td><td width=25%>"
+						+ endpoint + "</td></tr>";
 			} catch (Exception e) {
 				System.out.println("Error printing " + hid);
 			}
 		}
-		response.getWriter().println(s);						
-		response.getWriter().println("</table>");
-		
-		/* Print All HIDs. */
-		response.getWriter().println("<table border=1 class=\"stats\" width=100%>"
-			+ "<tr><td class=\"hed\" width=25%>HID</td>"
-			+ "<td class=\"hed\" width=25%>DESCRIPTION</td>"
-			+ "<td class=\"hed\" width=25%>HOST</td>"
-			+ "<td class=\"hed\" width=25%>ENDPOINT</td></tr>");
-		s = "";
-		hids = identityManager.getAllHIDs();
-		it = hids.iterator();
-		
-		while (it.hasNext()) {
-			HID hid = new HID(it.next().getHID());
-			try {
-				endpoint = this.serviceRegistry.getServiceURL(hid).toString();
-				if (endpoint == " ") {
-					endpoint = "-";
-				}
-				description = identityManager.getHIDInfo(hid).getDescription();
-				route = this.backboneRouter.getRoute(hid);
-				s = s + "<tr><td width=25%>" + hid.toString() 
-					+ "</td><td width=25%>" + description
-					+ "</td><td width=25%>" + route
-					+ "</td><td width=25%>" + endpoint + "</td></tr>";
-			}
-			catch (Exception e) {
-				System.out.println("Error printing " + hid);
-			}
-		}
-		response.getWriter().println(s);
-		response.getWriter().println("</table></body></html>");
+		return s;
+	}
+
+	private String buildHidTable(String title, Set<HIDInfo> hids,
+			boolean checkAttributes) {
+		return "<table border=1 class=\"stats\" "
+				+ "width=100%><tr><td class=\"hed\" width=25%>"
+				+ title
+				+ "</td>"
+				+ "<td class=\"hed\" width=25%>DESCRIPTION</td><td class=\"hed\" "
+				+ "width=25%>HOST</td><td class=\"hed\" width=25%>ENDPOINT</td></tr>\n"
+				+ processHIDs(hids, checkAttributes)
+				+ "\n</table></body></html>";
 	}
 
 }
