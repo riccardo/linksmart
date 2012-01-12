4d3b760d480d3257a0212803f03e87d875fae0e1
schneider-fit <schneider-fit@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2012-01-12 09:45:53 +0000

    Added folder for tests.
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@349 95b9cf05-cb7e-4607-9643-a8f9ddef502b

---
+++ components/NetworkManager2.0/BackboneJXTA/test/eu/linksmart/network/backbone/impl/jxta/BackboneJXTAImplTest.java
@@ -0,0 +1,29 @@
+package eu.linksmart.network.backbone.impl.jxta;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BackboneJXTAImplTest {
+
+	@Test
+	public void testSendData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReceiveData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBroadcastData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDestinationAddressAsString() {
+		fail("Not yet implemented");
+	}
+
+}
