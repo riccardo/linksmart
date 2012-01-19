4e871bf0c4cb630f8bff116e7966f566c6aacd91
tranten <tranten@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2012-01-19 14:26:18 +0000

    Show provided security properties on webconfiguration page
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@367 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/NetworkManager2.0/CommunicationSecurityManagerSym/src/eu/linksmart/security/communication/impl/sym/CommunicationSecurityManagerConfigurator.java
+++ components/NetworkManager2.0/CommunicationSecurityManagerSym/src/eu/linksmart/security/communication/impl/sym/CommunicationSecurityManagerConfigurator.java
@@ -1,9 +1,14 @@
 package eu.linksmart.security.communication.impl.sym;
 
 import java.util.Hashtable;
+import java.util.List;
+import java.util.Properties;
+
 import org.apache.log4j.Logger;
 import org.osgi.framework.BundleContext;
 
+import eu.linksmart.security.communication.CommunicationSecurityManager;
+import eu.linksmart.security.communication.SecurityProperty;
 import eu.linksmart.utils.Configurator;
 
 /**
@@ -15,27 +20,31 @@ import eu.linksmart.utils.Configurator;
 public class CommunicationSecurityManagerConfigurator extends Configurator {
 
 	/**
-	 * Unique identity under which the configuraton is registered
+	 * Unique identity under which the configuraton is registered.
 	 */
 	protected final static String COMSECMGR_PID = "eu.linksmart.security.communication.sym";
 	/**
-	 * Path to the default configurations
+	 * Identifier to the property provided properties.
+	 */
+	public static String SECURITY_PROVIDED = "CommunicationSecurityManager.providedSecurityProperties";
+	/**
+	 * Path to the default configurations.
 	 */
 	public final static String CONFIGURATION_FILE = "/resources/COMSECMGR.properties";
 	/**
-	 * Identifier to the property threshold as used by {@link CommunicationSecurityManager}
+	 * Identifier to the property threshold as used by {@link CommunicationSecurityManager}.
 	 */
 	public final static String TRUST_THRESHOLD = "CommunicationSecurityManager.trustThreshold";
 	/**
-	 * Identifier to the property TrustManager URL as used by {@link CommunicationSecurityManager}
+	 * Identifier to the property TrustManager URL as used by {@link CommunicationSecurityManager}.
 	 */
 	public final static String TRUST_MANAGER_URL = "CommunicationSecurityManager.trustManagerURL";
 	/**
-	 * Log4j logger of this class
+	 * Log4j logger of this class.
 	 */
 	private static Logger logger = Logger.getLogger(SecurityProtocolImpl.class);
 	/**
-	 * Implementation reference to be configured by this
+	 * Implementation reference to be configured by this.
 	 */
 	private CommunicationSecurityManagerImpl comSecMgr = null;
 	/*
@@ -49,6 +58,19 @@ public class CommunicationSecurityManagerConfigurator extends Configurator {
 				COMSECMGR_PID, CONFIGURATION_FILE);
 		this.context = context;
 		this.comSecMgr = comSecMgr;
+		//show provided services in configuration
+		StringBuilder sb = new StringBuilder();
+		List<SecurityProperty> securityProps = comSecMgr.getProperties();
+		int i = 0;
+		for(SecurityProperty prop : securityProps){
+			i++;
+			if (i == securityProps.size()) {
+				sb.append(prop.name());
+			} else {
+				sb.append(prop.name() + ", ");
+			}
+		}
+		this.setConfiguration(SECURITY_PROVIDED, sb.toString());
 	}
 
 	public void applyConfigurations(Hashtable updates) {
