ff0e3bdb85bc855b4df14ee79a9b346e845c483e
linksmart-dev <linksmart-dev@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2012-02-17 14:55:44 +0000

    Restructured NMResponse into status and message
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@417 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/NMResponse.java
+++ components/LinkSmartMiddlewareAPI/src/eu/linksmart/network/NMResponse.java
@@ -1,145 +1,100 @@
-/*
- * In case of German law being applicable to this license agreement, the following warranty and liability terms shall apply:
- *
- * 1. Licensor shall be liable for any damages caused by wilful intent or malicious concealment of defects.
- * 2. Licensor's liability for gross negligence is limited to foreseeable, contractually typical damages.
- * 3. Licensor shall not be liable for damages caused by slight negligence, except in cases 
- *    of violation of essential contractual obligations (cardinal obligations). Licensee's claims for 
- *    such damages shall be statute barred within 12 months subsequent to the delivery of the software.
- * 4. As the Software is licensed on a royalty free basis, any liability of the Licensor for indirect damages 
- *    and consequential damages - except in cases of intent - is excluded.
- *
- * This limitation of liability shall also apply if this license agreement shall be subject to law 
- * stipulating liability clauses corresponding to German law.
- */
 /**
- * Copyright (C) 2006-2010 [Telefonica I+D]
- *                         the HYDRA consortium, EU project IST-2005-034891
+ * NMResponse.java
  *
- * This file is part of LinkSmart.
- *
- * LinkSmart is free software: you can redistribute it and/or modify
- * it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE
- * version 3 as published by the Free Software Foundation.
- *
- * LinkSmart is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with LinkSmart.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/**
  * This file was auto-generated from WSDL
  * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
  */
 
 package eu.linksmart.network;
 
-/**
- * Network Manager response type
- */
-public class NMResponse  {
+public class NMResponse  implements java.io.Serializable {
+	public static final int STATUS_SUCCESS = 0;
+	public static final int STATUS_ERROR = 1;
+	public static final int STATUS_TIMEOUT = 2;
 	
-	public static final String  STATUS_SUCCESS = "SUCCESS";
-	public static final String  STATUS_TIMEOUT = "TIMEOUT";
-	public static final String  STATUS_ERROR = "ERROR";
-	
-	private java.lang.String data;
+    private java.lang.String message;
+    private int status;
 
-	private java.lang.Object __equalsCalc = null;
-	private boolean __hashCodeCalc = false;
+    public NMResponse() {
+    }
 
-	/**
-	 * Default constructor
-	 */
-	public NMResponse() {}
-	
-	/**
-	 * Constructor
-	 * 
-	 * @param data the data value for this NMResponse
-	 * @param sessionID the sessionID value for this NMResponse
-	 */
-	public NMResponse(java.lang.String data) {
-		this.data = data;		
-	}
+    public NMResponse(int status) {
+           this.status = status;
+    }
 
-	/**
-	 * Gets the data value for this NMResponse
-	 * 
-	 * @return the data value for this NMResponse
-	 */
-	public java.lang.String getData() {
-		return data;
-	}
 
-	/**
-	 * Sets the data value for this NMResponse
-	 * 
-	 * @param data the data value for this NMResponse
-	 */
-	public void setData(java.lang.String data) {
-		this.data = data;
-	}
+    /**
+     * Gets the message value for this NMResponse.
+     * 
+     * @return message
+     */
+    public java.lang.String getMessage() {
+        return message;
+    }
 
-	/**
-	 * Returns true if the object "obj" is "equal to" this one.
-	 * 
-	 * @param obj the object to compare
-	 * @return true if the object "obj" is "equal to" this one 
-	 */
-	public synchronized boolean equals(java.lang.Object obj) {
-		if (!(obj instanceof NMResponse)) {
-			return false;
-		}
-		
-		NMResponse other = (NMResponse) obj;
-		if (obj == null) {
-			return false;
-		}
-		if (this == obj) {
-			return true;
-		}
-		if (__equalsCalc != null) {
-			return (__equalsCalc == obj);
-		}
-		
-		__equalsCalc = obj;
-		boolean _equals;
-		_equals = true 
-			&& (((this.data == null) && (other.getData() == null))
-				|| ((this.data != null) && this.data.equals(other.getData())));
-		__equalsCalc = null;
-		return _equals;
-	}
 
-	/**
-	 * Returns a hash code value for the object
-	 * 
-	 * @return a hash code value for the object
-	 */
-	public synchronized int hashCode() {
-		if (__hashCodeCalc) {
-			return 0;
-		}
-		__hashCodeCalc = true;
-		int _hashCode = 1;
-		if (getData() != null) {
-			_hashCode += getData().hashCode();
-		}
-		__hashCodeCalc = false;
-		return _hashCode;
-	}
-	
-	
-	/**
-	 * Override default toString-method to display useful information (e.g. Data)
-	 */
-	public String toString() {
-		return getData() == null ? "NMResponse - No data available." : getData();
-	}
+    /**
+     * Sets the message value for this NMResponse.
+     * 
+     * @param message
+     */
+    public void setMessage(java.lang.String message) {
+        this.message = message;
+    }
+
+
+    /**
+     * Gets the status value for this NMResponse.
+     * 
+     * @return status
+     */
+    public int getStatus() {
+        return status;
+    }
+
+
+    /**
+     * Sets the status value for this NMResponse.
+     * 
+     * @param status
+     */
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof NMResponse)) return false;
+        NMResponse other = (NMResponse) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.message==null && other.getMessage()==null) || 
+             (this.message!=null &&
+              this.message.equals(other.getMessage()))) &&
+            this.status == other.getStatus();
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getMessage() != null) {
+            _hashCode += getMessage().hashCode();
+        }
+        _hashCode += getStatus();
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
 
 }
