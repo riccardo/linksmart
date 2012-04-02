34df24863ff6833b62173e3ffaf6fb22f6782bd5
hansmuff2 <hansmuff2@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2012-04-02 15:31:26 +0000

    new interface of CNET Event Manager
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/branches/1.2@461 95b9cf05-cb7e-4607-9643-a8f9ddef502b

---
+++ components/LinkSmartMiddlewareAPI/src/eu/linksmart/eventmanager/EventManagerPort.java
@@ -0,0 +1,21 @@
+/**
+ * EventManagerPort.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package eu.linksmart.eventmanager;
+
+public interface EventManagerPort extends java.rmi.Remote {
+    public boolean subscribe(java.lang.String topic, java.lang.String endpoint, int priority) throws java.rmi.RemoteException;
+    public boolean unsubscribe(java.lang.String topic, java.lang.String endpoint) throws java.rmi.RemoteException;
+    public boolean subscribeWithHID(java.lang.String topic, java.lang.String hid, int priority) throws java.rmi.RemoteException;
+    public boolean unsubscribeWithHID(java.lang.String topic, java.lang.String hid) throws java.rmi.RemoteException;
+    public eu.linksmart.eventmanager.Subscription[] getSubscriptions() throws java.rmi.RemoteException;
+    public boolean clearSubscriptions(java.lang.String endpoint) throws java.rmi.RemoteException;
+    public boolean clearSubscriptionsWithHID(java.lang.String hid) throws java.rmi.RemoteException;
+    public boolean setPriority(java.lang.String in0, int in1) throws java.rmi.RemoteException;
+    public boolean triggerRetryQueue() throws java.rmi.RemoteException;
+    public boolean publish(java.lang.String topic, eu.linksmart.eventmanager.Part[] in1) throws java.rmi.RemoteException;
+}
---
+++ components/LinkSmartMiddlewareAPI/src/eu/linksmart/eventmanager/Part.java
@@ -0,0 +1,104 @@
+/**
+ * Part.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package eu.linksmart.eventmanager;
+
+public class Part  implements java.io.Serializable {
+    private java.lang.String key;
+
+    private java.lang.String value;
+
+    public Part() {
+    }
+
+    public Part(
+           java.lang.String key,
+           java.lang.String value) {
+           this.key = key;
+           this.value = value;
+    }
+
+
+    /**
+     * Gets the key value for this Part.
+     * 
+     * @return key
+     */
+    public java.lang.String getKey() {
+        return key;
+    }
+
+
+    /**
+     * Sets the key value for this Part.
+     * 
+     * @param key
+     */
+    public void setKey(java.lang.String key) {
+        this.key = key;
+    }
+
+
+    /**
+     * Gets the value value for this Part.
+     * 
+     * @return value
+     */
+    public java.lang.String getValue() {
+        return value;
+    }
+
+
+    /**
+     * Sets the value value for this Part.
+     * 
+     * @param value
+     */
+    public void setValue(java.lang.String value) {
+        this.value = value;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof Part)) return false;
+        Part other = (Part) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.key==null && other.getKey()==null) || 
+             (this.key!=null &&
+              this.key.equals(other.getKey()))) &&
+            ((this.value==null && other.getValue()==null) || 
+             (this.value!=null &&
+              this.value.equals(other.getValue())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getKey() != null) {
+            _hashCode += getKey().hashCode();
+        }
+        if (getValue() != null) {
+            _hashCode += getValue().hashCode();
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+}
