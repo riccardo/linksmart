643ad156d3cb4476386f7cf48a9d661147fcca86
hansmuff2 <hansmuff2@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2012-04-26 09:44:15 +0000

    Exception handling. Removed some general catch Exception blocks.
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@495 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/NetworkManager2.0/NetworkManager/src/eu/linksmart/network/connection/Connection.java
+++ components/NetworkManager2.0/NetworkManager/src/eu/linksmart/network/connection/Connection.java
@@ -3,6 +3,7 @@ package eu.linksmart.network.connection;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.InvalidPropertiesFormatException;
 import java.util.Iterator;
 import java.util.Properties;
 
@@ -10,7 +11,6 @@ import org.apache.log4j.Logger;
 
 import eu.linksmart.network.HID;
 import eu.linksmart.network.Message;
-import eu.linksmart.network.networkmanager.core.impl.NetworkManagerCoreImpl;
 import eu.linksmart.security.communication.CommunicationSecurityManager;
 import eu.linksmart.security.communication.CryptoException;
 import eu.linksmart.security.communication.SecurityProtocol;
@@ -18,10 +18,10 @@ import eu.linksmart.security.communication.VerificationFailureException;
 import eu.linksmart.utils.Base64;
 
 /**
- * Holds properties and objects relevant for a connection
- * between two HIDs.
+ * Holds properties and objects relevant for a connection between two HIDs.
+ * 
  * @author Vinkovits
- *
+ * 
  */
 public class Connection {
 
@@ -51,127 +51,144 @@ public class Connection {
 	 */
 	private HID serverHID = null;
 
-	protected Connection(HID clientHID){
-		if(clientHID == null){
-			throw new IllegalArgumentException("Cannot set null for required fields.");
+	protected Connection(HID clientHID) {
+		if (clientHID == null) {
+			throw new IllegalArgumentException(
+					"Cannot set null for required fields.");
 		}
 		this.clientHID = clientHID;
 	}
-	
-	public Connection(HID clientHID, HID serverHID){
-		if(clientHID == null || serverHID == null){
-			throw new IllegalArgumentException("Cannot set null for required fields.");
+
+	public Connection(HID clientHID, HID serverHID) {
+		if (clientHID == null || serverHID == null) {
+			throw new IllegalArgumentException(
+					"Cannot set null for required fields.");
 		}
 		this.clientHID = clientHID;
 		this.serverHID = serverHID;
 	}
 
-	public HID getClientHID(){
+	public HID getClientHID() {
 		return clientHID;
 	}
 
-	public HID getServerHID(){
+	public HID getServerHID() {
 		return serverHID;
 	}
 
 	/**
-	 * Set {@link SecurityProtocol} for this connection. It has
-	 * to be ensured that no message is processed
-	 * until no security protocol is set for connection.
-	 * @param secProt Connection specific object
+	 * Set {@link SecurityProtocol} for this connection. It has to be ensured
+	 * that no message is processed until no security protocol is set for
+	 * connection.
+	 * 
+	 * @param secProt
+	 *            Connection specific object
 	 */
-	protected void setSecurityProtocol(SecurityProtocol secProt){
+	protected void setSecurityProtocol(SecurityProtocol secProt) {
 		securityProtocol = secProt;
 	}
 
 	/**
 	 * Creates a Message object for received data
-	 * @param data Data received over network
+	 * 
+	 * @param data
+	 *            Data received over network
 	 * @return Message object for further processing
 	 */
-	public Message processData(HID senderHID, HID receiverHID, byte[] data){
+	public Message processData(HID senderHID, HID receiverHID, byte[] data) {
 		Message tempMsg = null;
-		if(securityProtocol != null && securityProtocol.isInitialized()){
-			//if protocol is initialized than open message with it
-			try{
-				tempMsg = new Message("CIPHERTEXT", senderHID, receiverHID, data);
+		if (securityProtocol != null && securityProtocol.isInitialized()) {
+			// if protocol is initialized than open message with it
+			try {
+				tempMsg = new Message("CIPHERTEXT", senderHID, receiverHID,
+						data);
 				tempMsg = securityProtocol.unprotectMessage(tempMsg);
-			}catch(Exception e){
-				logger.debug("Cannot unprotect message from HID: " + senderHID.toString());
+			} catch (Exception e) {
+				logger.debug("Cannot unprotect message from HID: "
+						+ senderHID.toString());
 				return null;
 			}
-		} else if(securityProtocol != null && !securityProtocol.isInitialized()){
-			//if protocol not initialized then pass it for processing
+		} else if (securityProtocol != null
+				&& !securityProtocol.isInitialized()) {
+			// if protocol not initialized then pass it for processing
 			try {
-				//in this case the data is a serialized properties object
+				// in this case the data is a serialized properties object
 				Properties properties = new Properties();
 				properties.loadFromXML(new ByteArrayInputStream(data));
-				tempMsg = new Message(
-						(String)properties.remove(TOPIC),
-						senderHID,
-						receiverHID,
-						((String)properties.remove(APPLICATION_DATA)).getBytes()
-				);
+				tempMsg = new Message((String) properties.remove(TOPIC),
+						senderHID, receiverHID, ((String) properties
+								.remove(APPLICATION_DATA)).getBytes());
 				tempMsg = securityProtocol.processMessage(tempMsg);
 				return tempMsg;
 			} catch (CryptoException e) {
-				logger.warn("Error during cryptographic operation",e);
+				logger.error("Error during cryptographic operation", e);
 			} catch (VerificationFailureException e) {
-				logger.debug("Signature is not valid from HID: " + senderHID.toString());
+				logger.error("Signature is not valid from HID: "
+						+ senderHID.toString());
 			} catch (IOException e) {
-				logger.warn("Cannot parse message from HID: " + senderHID.toString() + ": " + new String(data));
+				logger.error("Cannot parse message from HID: "
+						+ senderHID.toString() + ":\n " + new String(data));
 			}
 			return null;
-		}	
-
-		try{
-			//open data and divide it into properties of the message and application data
-			Properties properties = new Properties();
-			properties.loadFromXML(new ByteArrayInputStream((tempMsg == null)? data : tempMsg.getData()));
-
-			//create real message
-			Message message = new Message(
-					(String)properties.remove(TOPIC),
-					senderHID,
-					receiverHID,
-					(Base64.decode((String)properties.remove(APPLICATION_DATA))));
-			//go through the properties and add them to the message
-			Iterator<Object> i = properties.keySet().iterator();
-			while(i.hasNext()){
-				String key = (String)i.next();
-				message.setProperty(key, properties.getProperty(key));
-			}
-			return message;
-		}catch(Exception e){
-			logger.debug("Cannot parse message from HID: " + senderHID.toString());
 		}
-		return null;
+
+		// open data and divide it into properties of the message and
+		// application data
+		Properties properties = new Properties();
+		try {
+			properties.loadFromXML(new ByteArrayInputStream(
+					(tempMsg == null) ? data : tempMsg.getData()));
+		} catch (InvalidPropertiesFormatException e) {
+			logger.error(
+					"Unable to load properties from XML data. Data is not valid XML: "
+							+ new String(data), e);
+		} catch (IOException e) {
+			logger.error("Unable to load properties from XML data: "
+					+ new String(data), e);
+		}
+
+		// create real message
+		Message message = new Message((String) properties.remove(TOPIC),
+				senderHID, receiverHID, (Base64.decode((String) properties
+						.remove(APPLICATION_DATA))));
+		// go through the properties and add them to the message
+		Iterator<Object> i = properties.keySet().iterator();
+		while (i.hasNext()) {
+			String key = (String) i.next();
+			message.setProperty(key, properties.getProperty(key));
+		}
+		return message;
 	}
 
 	/**
-	 * Creates a serialized representation of the
-	 * Message object which can be sent over network
-	 * @param msg Message to convert
+	 * Creates a serialized representation of the Message object which can be
+	 * sent over network
+	 * 
+	 * @param msg
+	 *            Message to convert
 	 * @return Serialized version of the message including all properties
-	 * @throws Exception When message cannot be processed for sending
+	 * @throws Exception
+	 *             When message cannot be processed for sending
 	 */
-	public byte[] processMessage(Message msg) throws Exception{
-		if(securityProtocol != null && !securityProtocol.isInitialized()){
-			//set the message to be sent by security protocol
+	public byte[] processMessage(Message msg) throws Exception {
+		if (securityProtocol != null && !securityProtocol.isInitialized()) {
+			// set the message to be sent by security protocol
 			msg = securityProtocol.processMessage(msg);
-			//the message has to be processed for sending now by the regular code
+			// the message has to be processed for sending now by the regular
+			// code
 		}
-		//read the properties of the message and put them into one properties object
+		// read the properties of the message and put them into one properties
+		// object
 		Properties props = new Properties();
 		Iterator<String> i = msg.getKeySet().iterator();
-		while(i.hasNext()){
+		while (i.hasNext()) {
 			String key = i.next();
 			props.put(key, msg.getProperty(key));
 		}
-		//put application data into properties
+		// put application data into properties
 		props.put(APPLICATION_DATA, Base64.encodeBytes(msg.getData()));
 		props.put(TOPIC, msg.getTopic());
-		//convert props into xml and encode it
+		// convert props into xml and encode it
 		ByteArrayOutputStream bos = new ByteArrayOutputStream();
 		byte[] serializedCommand = null;
 		try {
@@ -179,23 +196,26 @@ public class Connection {
 			serializedCommand = bos.toByteArray();
 		} catch (IOException e) {
 			logger.warn("Message to be sent cannot be parsed!");
-		} finally{
+		} finally {
 			try {
 				bos.close();
 			} catch (IOException e) {
-				logger.error("Error closing stream",e);
+				logger.error("Error closing stream", e);
 			}
 		}
-		if(securityProtocol != null 
-				&& securityProtocol.isInitialized() 
-				&& props.getProperty(TOPIC).equals(CommunicationSecurityManager.SECURITY_PROTOCOL_TOPIC)){
-			/*this could also be a message which has been stored by the security protocol
-			* until now and is becoming sent at last */
-			//set all data of the message as data part and protect it
+		if (securityProtocol != null
+				&& securityProtocol.isInitialized()
+				&& props.getProperty(TOPIC).equals(
+						CommunicationSecurityManager.SECURITY_PROTOCOL_TOPIC)) {
+			/*
+			 * this could also be a message which has been stored by the
+			 * security protocol until now and is becoming sent at last
+			 */
+			// set all data of the message as data part and protect it
 			msg.setData(serializedCommand);
 			msg = securityProtocol.protectMessage(msg);
 			return msg.getData();
-		}else{
+		} else {
 			return serializedCommand;
 		}
 	}
