b9c165995df1eb81e8b8497b8f1913a00d9b9e92
hansmuff2 <hansmuff2@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2012-05-15 15:00:24 +0000

    Initial Commit of RemoteServiceStore
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/branches/1.2@528 95b9cf05-cb7e-4607-9643-a8f9ddef502b

---
+++ components/LinkSmartRemoteServiceStore/src/eu/linksmart/remote/service/store/LinkSmartRemoteServiceStore.java
@@ -0,0 +1,22 @@
+package eu.linksmart.remote.service.store;
+
+import java.util.Map;
+
+public interface LinkSmartRemoteServiceStore {
+
+	public Object getRemoteHydraServiceByPID(String pid, String className) throws Exception;
+
+	public Object getRemoteHydraServiceByDescription(String description, String className) throws Exception;
+
+	/**
+	 * @param query  The format of the query is supposed be like this:
+	 * (key1==cond1)&&(key2==cond2*)||(key3==cond3)...</code>
+	 * 
+	 */
+	public String getServiceCryptoHID(String query);
+	
+	/**
+	 * @param attributes Method connects key values pairs with && operand
+	 */
+	public String getHydraServiceHIDByAttributes(Map <String,String> attributes);
+}
