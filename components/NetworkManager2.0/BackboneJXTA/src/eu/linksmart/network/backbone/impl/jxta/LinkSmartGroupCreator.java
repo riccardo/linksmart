e8f02d80164c4cf7454ed28669223c594c2d67a4
likho <likho@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2012-11-07 17:00:10 +0000

    not UTF character fixed
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@679 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/NetworkManager2.0/BackboneJXTA/src/eu/linksmart/network/backbone/impl/jxta/LinkSmartGroupCreator.java
+++ components/NetworkManager2.0/BackboneJXTA/src/eu/linksmart/network/backbone/impl/jxta/LinkSmartGroupCreator.java
@@ -269,7 +269,7 @@ public class LinkSmartGroupCreator {
 			TextElement loginElement = loginAndPasswd.createElement("login",
 				loginAndPasswdString);
 			loginAndPasswd.appendChild(loginElement);
-			/* Include it on PeerGroup¿dveertisement. */
+			/* Include it on PeerGroupAdvertisement. */
 			linksmartGroupAdvertisement.putServiceParam(PeerGroup.membershipClassID,
 				loginAndPasswd);
 		}
