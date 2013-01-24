ceefd6a781dceae1176f4bbf418f9273fae3e80f
hansmuff2 <hansmuff2@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2013-01-24 14:07:03 +0000

    EventManager v1.3
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/branches/1.3@750 95b9cf05-cb7e-4607-9643-a8f9ddef502b

---
+++ components/LinkSmartMiddlewareClients/src/eu/linksmart/eventmanager/client/EventManagerImplementation.java
@@ -0,0 +1,16 @@
+/**
+ * EventManagerImplementation.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package eu.linksmart.eventmanager.client;
+
+public interface EventManagerImplementation extends javax.xml.rpc.Service {
+    public java.lang.String getBasicHttpBinding_EventManagerPortAddress();
+
+    public eu.linksmart.eventmanager.EventManagerPort getBasicHttpBinding_EventManagerPort() throws javax.xml.rpc.ServiceException;
+
+    public eu.linksmart.eventmanager.EventManagerPort getBasicHttpBinding_EventManagerPort(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
