427a534e8fe9337a6605961f00d41f11acfffd19
tranten <tranten@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2013-04-11 12:07:22 +0000

    Fixit eclipse refactoring+svn fault
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@836 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/NetworkManager/SOAPTunneling/test/eu/linksmart/network/soaptunnel/SOAPTunnelServletTest.java
+++ components/NetworkManager/SOAPTunneling/test/eu/linksmart/network/soaptunnel/SOAPTunnelServletTest.java
@@ -1,4 +1,4 @@
-package eu.linksmart.network.tunnel;
+package eu.linksmart.network.soaptunnel;
 
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
@@ -19,51 +19,40 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import eu.linksmart.network.NMResponse;
-import eu.linksmart.network.Registration;
 import eu.linksmart.network.VirtualAddress;
 import eu.linksmart.network.networkmanager.core.NetworkManagerCore;
-import eu.linksmart.utils.Part;
 
-public class TunnelServletTest {
+public class SOAPTunnelServletTest {
 
 	private NetworkManagerCore nmCore;
-	private TunnelServlet tunnelServlet;
+	private SOAPTunnelServlet soapTunnelServlet;
 	private HttpServletRequest requestShort;
 	private HttpServletRequest requestLong;
 	private HttpServletRequest requestWsdl;
+	private HttpServletRequest requestBad;
 	private HttpServletResponse response;
 	private String receiverString = "0.0.0.6986094776732394497";
-	private String urlShort = "/0?description=\"CalculatorForBeginners\"";
-	private String urlLong = "/0.0.0.1/some/addition?description=\"CalculatorForBeginners\"&pid=\"010\"#default";
-	private String urlWsdl = "/0/wsdl?description=\"CalculatorForBeginners\"";
+	private String urlShort = "/0/0.0.0.6986094776732394497";
+	private String urlLong = "/0/0.0.0.6986094776732394497/hola";
+	private String urlWsdl = "/0/0.0.0.6986094776732394497/wsdl";
+	private String urlBad = "kaputto";
 	private VirtualAddress nmAddress;
 	private String nmAddressString = "0.0.0.0";
 	private NMResponse nmResponse;
 	private PrintWriter pWriter;
-	private Tunnel tunnel;
-	private Part[] descriptionOnly;
-	private Part[] descNPid;
 
 
 	@Before
 	public void setUp(){
 		this.nmCore = mock(NetworkManagerCore.class);
-		this.tunnel = mock(Tunnel.class);
-		
-		this.tunnelServlet = new TunnelServlet(this.tunnel);
+		this.soapTunnelServlet = new SOAPTunnelServlet(nmCore);
 		this.response = mock(HttpServletResponse.class);
 		this.pWriter = new PrintWriter(new ByteArrayOutputStream());
 		try {
-			when(tunnel.getNM()).thenReturn(this.nmCore);
 			when(response.getWriter()).thenReturn(pWriter);
 		} catch (IOException e1) {
 			//NOP
 		}
-		
-		this.descriptionOnly = new Part[]{new Part("DESCRIPTION", "CalculatorForBeginners")};
-		this.descNPid = new Part[]{
-				new Part("DESCRIPTION", "CalculatorForBeginners"),
-				new Part("PID", "010")};
 		this.nmAddress = new VirtualAddress(nmAddressString);
 		this.nmResponse = new NMResponse(NMResponse.STATUS_SUCCESS);
 		this.nmResponse.setMessage("Bla");
@@ -79,48 +68,41 @@ public class TunnelServletTest {
 		} catch (RemoteException e) {
 			//NOP
 		}
-		
-		try {
-			when(this.nmCore.getServiceByAttributes(any(Part[].class))).
-			thenReturn(new Registration[]{
-					new Registration(new VirtualAddress(this.receiverString), "CalculatorForBeginners")});
-		} catch (RemoteException e) {
-			//NOP
-		}
 
 		this.requestShort = mock(HttpServletRequest.class);
 		when(this.requestShort.getHeaderNames()).thenReturn(new Hashtable<String, String>().elements());
-		when(this.requestShort.getPathInfo()).thenReturn(urlShort.substring(0, urlShort.indexOf("?")));
-		when(this.requestShort.getMethod()).thenReturn("POST");
+		when(this.requestShort.getPathInfo()).thenReturn(urlShort);
+		when(this.requestShort.getMethod()).thenReturn("GET");
 		when(this.requestShort.getProtocol()).thenReturn("HTTP/1.1");
-		when(this.requestShort.getQueryString()).thenReturn(this.urlShort.substring(urlShort.indexOf("?") + 1));
 		this.requestLong = mock(HttpServletRequest.class);
 		when(this.requestLong.getHeaderNames()).thenReturn(new Hashtable<String, String>().elements());
-		when(this.requestLong.getPathInfo()).thenReturn(urlLong.substring(0, urlLong.indexOf("?")));
-		when(this.requestLong.getMethod()).thenReturn("POST");
+		when(this.requestLong.getPathInfo()).thenReturn(urlLong);
+		when(this.requestLong.getMethod()).thenReturn("GET");
 		when(this.requestLong.getProtocol()).thenReturn("HTTP/1.1");
-		when(this.requestLong.getQueryString()).thenReturn(this.urlLong.substring(urlLong.indexOf("?") + 1));
 		this.requestWsdl = mock(HttpServletRequest.class);
 		when(this.requestWsdl.getHeaderNames()).thenReturn(new Hashtable<String, String>().elements());
-		when(this.requestWsdl.getPathInfo()).thenReturn(urlWsdl.substring(0, urlWsdl.indexOf("?")));
+		when(this.requestWsdl.getPathInfo()).thenReturn(urlWsdl);
 		when(this.requestWsdl.getMethod()).thenReturn("GET");
 		when(this.requestWsdl.getProtocol()).thenReturn("HTTP/1.1");
-		when(this.requestWsdl.getQueryString()).thenReturn(this.urlWsdl.substring(urlWsdl.indexOf("?") + 1));
+		this.requestBad = mock(HttpServletRequest.class);
+		when(this.requestBad.getHeaderNames()).thenReturn(new Hashtable<String, String>().elements());
+		when(this.requestBad.getPathInfo()).thenReturn(urlBad);
+		when(this.requestBad.getMethod()).thenReturn("GET");
+		when(this.requestBad.getProtocol()).thenReturn("HTTP/1.1");
 	}
 
 	@Test
 	public void testValidURLShort(){	
 		try {
-			this.tunnelServlet.doPost(requestShort, response);
+			this.soapTunnelServlet.doPost(requestShort, response);
 		} catch (IOException e1) {
 			// NOP
 		}
 		try {
-			Mockito.verify(nmCore).getServiceByAttributes(this.descriptionOnly);
 			Mockito.verify(nmCore).sendData(
 					nmAddress,
 					new VirtualAddress(receiverString),
-					new String("POST / HTTP/1.1\r\n\r\n").getBytes(),
+					new String("\r\n").getBytes(),
 					true);
 		} catch (RemoteException e) {
 			//NOP
@@ -130,16 +112,15 @@ public class TunnelServletTest {
 	@Test
 	public void testValidURLong(){	
 		try {
-			this.tunnelServlet.doPost(requestLong, response);
+			this.soapTunnelServlet.doPost(requestLong, response);
 		} catch (IOException e1) {
 			// NOP
 		}
 		try {
-			Mockito.verify(nmCore).getServiceByAttributes(this.descNPid);
 			Mockito.verify(nmCore).sendData(
-					new VirtualAddress("0.0.0.1"),
+					nmAddress,
 					new VirtualAddress(receiverString),
-					new String("POST /some/addition HTTP/1.1\r\n\r\n").getBytes(),
+					new String("\r\n").getBytes(),
 					true);
 		} catch (RemoteException e) {
 			//NOP
@@ -148,14 +129,13 @@ public class TunnelServletTest {
 
 
 	@Test
-	public void testValidUrlWsdl(){	
+	public void testValidURWsdl(){	
 		try {
-			this.tunnelServlet.doGet(requestWsdl, response);
+			this.soapTunnelServlet.doGet(requestWsdl, response);
 		} catch (IOException e1) {
 			// NOP
 		}
 		try {
-			Mockito.verify(nmCore).getServiceByAttributes(this.descriptionOnly);
 			Mockito.verify(nmCore).sendData(
 					nmAddress,
 					new VirtualAddress(receiverString),
@@ -163,6 +143,16 @@ public class TunnelServletTest {
 					true);
 		} catch (RemoteException e) {
 			//NOP
+		}
+	}
+
+	@Test
+	public void testValidURLBad(){	
+		try {
+			this.soapTunnelServlet.doPost(requestBad, response);
+			Mockito.verify(response).setStatus(HttpServletResponse.SC_NOT_FOUND);
+		} catch (IOException e1) {
+			//successful test
 		}
 	}
 }
