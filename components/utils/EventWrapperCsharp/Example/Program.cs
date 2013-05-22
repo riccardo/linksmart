8ef34a65e5ef3b0a7003c22c1d270b7a8f6c7005
hansmuff2 <hansmuff2@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2013-05-22 09:40:20 +0000

    git-svn-id: http://svn.code.sf.net/p/linksmart/code/branches/1.3@898 95b9cf05-cb7e-4607-9643-a8f9ddef502b

---
+++ components/utils/EventWrapperCsharp/Example/Program.cs
@@ -0,0 +1,44 @@
+﻿using eu.linksmart.eventing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //example of subscribing events
+            EventSubscriberWrapper subscriber = new EventSubscriberWrapper("MyTestSubscriber", "http://localhost:8122/MyEventSubscriber");
+            subscriber.StartService();
+            EventSubscriberWrapper.NotifyDelegate callback = new EventSubscriberWrapper.NotifyDelegate(notify);
+            subscriber.SubscribeEvent("EventManager:FIT:Test", "EVENT/TEST", callback);
+
+
+            //example of publishing events
+            EventPublisherWrapper publisher = new EventPublisherWrapper();
+            eu.linksmart.eventing.EventManagerStub.Part p = new eu.linksmart.eventing.EventManagerStub.Part();
+            p.key="testKey";
+            p.value = "testValue";
+            eu.linksmart.eventing.EventManagerStub.Part[] parts = new eu.linksmart.eventing.EventManagerStub.Part[]{ p };
+
+            Console.ReadLine();
+            
+            //while(true){
+            //    publisher.PublishEvent("EventManager:FIT:Test", "EVENT/TEST", parts);
+              //  System.Threading.Thread.Sleep(2000);
+            //}            
+        }
+
+        static bool notify(string topic, eu.linksmart.eventing.EventManagerStub.Part[] parts) {
+            // recieve the events
+            Console.WriteLine(topic + " " + parts[0].key + " : " + parts[0].value);            
+            return true;
+        }
+    }
+}
