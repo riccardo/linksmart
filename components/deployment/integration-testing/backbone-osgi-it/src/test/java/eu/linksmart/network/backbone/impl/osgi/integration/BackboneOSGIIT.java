72845775f7aafb6fa630e4b0b5003b96bc0ee562
hrasheed <hrasheed@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2014-06-02 15:09:59 +0000

    feature name is updated, using it-utils dependency
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@1279 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/deployment/integration-testing/backbone-osgi-it/src/test/java/eu/linksmart/network/backbone/impl/osgi/integration/BackboneOSGIIT.java
+++ components/deployment/integration-testing/backbone-osgi-it/src/test/java/eu/linksmart/network/backbone/impl/osgi/integration/BackboneOSGIIT.java
@@ -1,96 +1,38 @@
 package eu.linksmart.network.backbone.impl.osgi.integration;
 
-import java.io.File;
 import java.util.List;
 
-import eu.linksmart.network.NMResponse;
+import eu.linksmart.it.utils.ITConfiguration;
 import eu.linksmart.network.VirtualAddress;
 import eu.linksmart.network.backbone.Backbone;
-import eu.linksmart.network.routing.BackboneRouter;
 import eu.linksmart.security.communication.SecurityProperty;
 import junit.framework.Assert;
-import org.junit.Ignore;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
-import org.ops4j.pax.exam.karaf.options.KarafDistributionOption;
-import org.ops4j.pax.exam.karaf.options.LogLevelOption;
-import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
-import org.ops4j.pax.exam.spi.reactors.PerMethod;
 
 import javax.inject.Inject;
 
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import static org.ops4j.pax.exam.CoreOptions.maven;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
 
-/**
- * Created with IntelliJ IDEA.
- * User: carlos
- * Date: 03.04.14
- * Time: 14:02
- * To change this template use File | Settings | File Templates.
- */
 @RunWith(PaxExam.class)
-
-@ExamReactorStrategy(PerMethod.class)
 public class  BackboneOSGIIT  {
-
-        private VirtualAddress receiverVirtualAddress = new VirtualAddress("354.453.455.323");
-        private VirtualAddress senderVirtualAddress = new VirtualAddress("354.453.993.323");
-        private String endpointSender = "endpoint_sender";
-        private String endpointReciever = "endpoint_reciever";
-
-        private byte[] revieveBuffer = {0,0};
 
         @Inject
         private Backbone backboneOSGI;
-        @Inject
-        private BackboneRouter backboneRouter;
-
+        
         @Configuration
         public Option[] config() {
-            return new Option[] {
-                    // Provision and launch a container based on a distribution of Karaf (Apache ServiceMix)
-                    karafDistributionConfiguration()
-                            .frameworkUrl(
-                                    maven()
-                                            .groupId("org.apache.servicemix")
-                                            .artifactId("apache-servicemix")
-                                            .type("zip")
-                                            .version("5.0.0"))
-                            .karafVersion("3.3.0")
-                            .name("Apache ServiceMix")
-                            .unpackDirectory(new File("target/servicemix-karaf"))
-                            .useDeployFolder(false),
-
-                    //KarafDistributionOption.debugConfiguration("5005", true) ,
-                    /*
-                    * keeping container sticks around after the test so we can check the contents
-                   // of the data directory when things go wrong.
-                    */
-                    keepRuntimeFolder(),
-                    /*
-                    * don't bother with local console output as it just ends up cluttering the logs
-                    */
-                    configureConsole().ignoreLocalConsole(),
-                    /*
-                    * force the log level to INFO so we have more details during the test. It defaults to WARN.
-                    */
-                    logLevel(LogLevelOption.LogLevel.INFO),
-                    /*
-                    * karaf feature will be provisioned to the test container from a local or remote Maven repository
-                    * using the standard Maven lookup and caching procedures
-                    */
-                    features("mvn:eu.linksmart/linksmart-features/2.2.0-SNAPSHOT/xml/features","backbone-osgi-it")
-
-
+        	return new Option[] {
+            		ITConfiguration.regressionDefaults(),
+            		features(ITConfiguration.getFeaturesRepositoryURL(),"backbone-osgi"),  
             };
         }
 
@@ -99,9 +41,7 @@ public class  BackboneOSGIIT  {
 
         // TEST #1
         // this test retrieves class name & list of security types
-
-
-
+    	
         String fromService = backboneOSGI.getName().toString();
         System.out.println("class name : "+fromService);
         Assert.assertEquals("eu.linksmart.network.backbone.impl.osgi.BackboneOsgiImpl",fromService);
