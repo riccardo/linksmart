77a5c50e41bca8067578456736a3fe4a54333e01
likho <likho@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2014-07-09 15:44:45 +0000

    jxta it switched from apache servicemix container to karaf 3.0.1 container
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@1433 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/deployment/integration-testing/backbone-jxta-it/src/test/java/eu/linksmart/network/backbone/impl/jxta/integration/BackboneJXTAIT.java
+++ components/deployment/integration-testing/backbone-jxta-it/src/test/java/eu/linksmart/network/backbone/impl/jxta/integration/BackboneJXTAIT.java
@@ -1,31 +1,25 @@
 package eu.linksmart.network.backbone.impl.jxta.integration;
 
-import java.io.File;
-import java.util.List;
-
+import eu.linksmart.it.utils.ITConfiguration;
 import eu.linksmart.network.VirtualAddress;
 import eu.linksmart.network.backbone.Backbone;
 import eu.linksmart.security.communication.SecurityProperty;
 import junit.framework.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
-import org.ops4j.pax.exam.karaf.options.LogLevelOption;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerMethod;
 
 import javax.inject.Inject;
+import java.util.List;
 
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import static org.ops4j.pax.exam.CoreOptions.maven;
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
 
 /**
  * Created with IntelliJ IDEA.
@@ -42,43 +36,16 @@ public class  BackboneJXTAIT  {
         @Inject
         private Backbone backboneJXTA;
 
-        @Configuration
-        public Option[] config() {
-            return new Option[] {
-                    // Provision and launch a container based on a distribution of Karaf (Apache ServiceMix)
-                    karafDistributionConfiguration()
-                            .frameworkUrl(
-                                    maven()
-                                            .groupId("org.apache.servicemix")
-                                            .artifactId("apache-servicemix")
-                                            .type("zip")
-                                            .version("5.0.0"))
-                            .karafVersion("3.3.0")
-                            .name("Apache ServiceMix")
-                            .unpackDirectory(new File("target/servicemix-karaf"))
-                            .useDeployFolder(false),
-                    /*
-                    * keeping container sticks around after the test so we can check the contents
-                   // of the data directory when things go wrong.
-                    */
-                    keepRuntimeFolder(),
-                    /*
-                    * don't bother with local console output as it just ends up cluttering the logs
-                    */
-                    configureConsole().ignoreLocalConsole(),
-                    /*
-                    * force the log level to INFO so we have more details during the test. It defaults to WARN.
-                    */
-                    logLevel(LogLevelOption.LogLevel.INFO),
-                    /*
-                    * karaf feature will be provisioned to the test container from a local or remote Maven repository
-                    * using the standard Maven lookup and caching procedures
-                    */
-                    features("mvn:eu.linksmart/linksmart-features/2.2.0-SNAPSHOT/xml/features","backbone-jxta-it")
-                    //features("mvn:eu.linksmart/jxta-integration-feature/1.0.0/xml/features","jxta-integration-feature")
 
-            };
-        }
+    @Configuration
+    public Option[] config() {
+        return new Option[] {
+                ITConfiguration.regressionDefaults(),
+                //features(ITConfiguration.getFeaturesRepositoryURL(),"jxta-integration-feature"),
+                features(ITConfiguration.getFeaturesRepositoryURL(),"backbone-jxta-it"),
+        };
+    }
+
 
     @Test
     public void basicIntegrationChain() throws InterruptedException {
