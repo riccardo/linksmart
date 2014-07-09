b7392a41321ca159a3e69d9566ff97d3b5ca0094
hrasheed <hrasheed@95b9cf05-cb7e-4607-9643-a8f9ddef502b>
2014-07-09 13:00:07 +0000

    test code updated
    git-svn-id: http://svn.code.sf.net/p/linksmart/code/trunk@1431 95b9cf05-cb7e-4607-9643-a8f9ddef502b

--- components/deployment/integration-testing/trust-manager-it/src/test/java/eu/linksmart/security/trustmanager/impl/integration/TrustManagerIT.java
+++ components/deployment/integration-testing/trust-manager-it/src/test/java/eu/linksmart/security/trustmanager/impl/integration/TrustManagerIT.java
@@ -7,58 +7,38 @@ import javax.inject.Inject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
 
-import org.apache.log4j.Logger;
-
 import eu.linksmart.it.utils.ITConfiguration;
-
 import eu.linksmart.security.trustmanager.TrustManager;
 
-
-/* 
- * @RunWith(PaxExam.class) hooks Pax Exam into JUnit and does its magic of 
- * setting up a test container with an OSGi framework and bundles under test
- */
 @RunWith(PaxExam.class)
 public class TrustManagerIT {
-
-
-    /*
-	 * @Inject triggers the injection of OSGi services into the test.
-	 * the test method can access the BundleContext of the probe bundle or any service obtained from the OSGi service registry
-	 */
+	
     @Inject
     private TrustManager trustManager;
 
-    /*
-     * @Configuration returns an array of configuration options. 
-     * These options define the set of bundles to be provisioned to the OSGi framework in the test container 
-     */
     @Configuration
     public Option[] config() {
         return new Option[] {
         		ITConfiguration.regressionDefaults(),
-        		features(ITConfiguration.getFeaturesRepositoryURL(),"trust-manager"),  // this feature will install all bundles including required dependencies without web-service provider                  
+        		features(ITConfiguration.getFeaturesRepositoryURL(),"trust-manager"),                    
         };
     }
     
     @Test
-    public void testService() throws Exception {
-    	
-
-        assertNotNull(trustManager.getClass());
-        String className = trustManager.getClass().getCanonicalName();
-        System.out.println("ClassName: "+className);
-        Assert.assertEquals("eu.linksmart.security.trustmanager.impl.TrustManagerImpl",className);
-         
+    public void testService() throws Exception { 
         try {
-        	assertTrue(true);
+        	System.out.println("starting trust-manager IT");
+        	assertNotNull(trustManager.getClass());
+            String className = trustManager.getClass().getCanonicalName();
+            System.out.println("ClassName: "+className);
+            Assert.assertEquals("eu.linksmart.security.trustmanager.impl.TrustManagerImpl",className);
+            System.out.println("trust-manager IT successfully completed");
         } catch(Exception e) {
         	fail(e.getMessage());
         }
