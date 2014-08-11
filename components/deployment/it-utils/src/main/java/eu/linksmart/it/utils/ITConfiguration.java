949bb5ef3e9923ca7057fe5e64911b9f25cc06b3
carlos <karl.catewicz@fit.fraunhofer.de>
2014-08-11 17:47:01 +0200

    - every usage will create an unique working directory - logging introduced

--- components/deployment/it-utils/src/main/java/eu/linksmart/it/utils/ITConfiguration.java
+++ components/deployment/it-utils/src/main/java/eu/linksmart/it/utils/ITConfiguration.java
@@ -1,5 +1,6 @@
 package eu.linksmart.it.utils;
 
+import org.apache.log4j.Logger;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.karaf.options.KarafDistributionOption;
 import org.ops4j.pax.exam.karaf.options.LogLevelOption.LogLevel;
@@ -8,6 +9,8 @@ import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
 import java.io.File;
 import java.lang.reflect.Field;
 import java.nio.charset.Charset;
+import java.util.UUID;
+
 
 import static org.ops4j.pax.exam.CoreOptions.*;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.*;
@@ -30,12 +33,16 @@ public class ITConfiguration {
 	private static int DEBUG_PORT = 8889;
 	
 	private static int HTTP_PORT = 8882;
+
+    private static Logger mLogger = Logger.getLogger((ITConfiguration.class.getName()));
 	
 	private static final String CORE_FEATURES_REPOSITORY_URL = "mvn:eu.linksmart.features/linksmart-features/" + LINKSMART_VERSION + "/xml/features";
 	
 	private static final String TESTING_FEATURES_REPOSITORY_URL = "mvn:eu.linksmart.features/linksmart-testing-features/" + LINKSMART_VERSION + "/xml/features";
 	
 	private static final String SERVICES_FEATURES_REPOSITORY_URL = "mvn:eu.linksmart.features/linksmart-services-features/" + LINKSMART_VERSION + "/xml/features";
+
+
 	
 	public static Option regressionDefaults() {
         return regressionDefaults(OSGI_CONTAINER_KARAF);
@@ -59,7 +66,16 @@ public class ITConfiguration {
     	setOSGiContainer(osgiContainerType);
     	DEBUG = debugFlag;
     	DEBUG_PORT = debugPort;
-    	
+
+        String workingDir;
+        if(unpackDir==null) {
+            workingDir = UUID.randomUUID().toString();
+            workingDir = "target/" + workingDir;
+            mLogger.debug("working directory :" + workingDir);
+        }else {
+            workingDir=unpackDir;
+        }
+
         return composite(
 
         		/*
@@ -70,9 +86,13 @@ public class ITConfiguration {
         			.frameworkUrl(mvnKarafDist())
         			.karafVersion(KARAF_DISTRO_VERSION)
         			.name(KARAF_DISTRO_NAME)
-        			.unpackDirectory(unpackDir == null ? null : new File(unpackDir))
+        			.unpackDirectory(workingDir == null ? null : new File(workingDir))
         			.useDeployFolder(false),
-            
+
+                /*
+                *  setup working directory
+                */
+                workingDirectory(workingDir),
                 /*
                  * keeping container sticks around after the test so we can check the contents
                    of the data directory when things go wrong.        
